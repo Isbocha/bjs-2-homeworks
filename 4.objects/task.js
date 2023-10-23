7b588223e159c90fd814ac0888324e2757e10db7
InnaB <isbocha@gmail.com>
2023-10-23 21:49:44 +0300

    Add  4.objects/task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.marks !== undefined) {
+      this.marks.push(...marksToAdd);
+    } 
+  }
+  Student.prototype.getAverage = function () {
+    if (this.marks.length === 0) {
+      return 0;
+    } else {
+      let result = this.marks.reduce((age, item, index, acc) => {
+        age += item;
+        if (index === acc.length - 1) {
+          return age / acc.length;
+        }
+        return age;
+      }, 0);
   
-}
-
-Student.prototype.getAverage = function () {
+      return result
+    }
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
